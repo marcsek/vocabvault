3fb1d6973c9277ec813dbd99b27b88acf0ff3c48
marcsek <jakubmarcek955@gmail.com>
2023-02-11 12:24:27 +0100

    icony

--- apps/client/src/pages/NewSession/components/SessionTypeSelector.tsx
+++ apps/client/src/pages/NewSession/components/SessionTypeSelector.tsx
@@ -1,6 +1,6 @@
 import ListBox from '@ui/ListBox';
 import React from 'react';
-import { RxInfoCircled } from 'react-icons/rx';
+import ThinkingIcon from '../../../assets/Icons/ThinkingIcon.svg';
 
 type TSessionType = { id: string; description: string };
 
@@ -15,9 +15,24 @@ const SessionTypeSelector = (props: Props) => {
     <div className="flex w-full flex-col gap-2">
       <ListBox {...props} fieldKey="id" fieldValue="id" label="Type of session" />
       <p className="flex items-center gap-2 text-sm leading-none text-gray-200">
-        <RxInfoCircled size={16} />
-        {props.value.description}
+        {/* <RxInfoCircled size={16} /> */}
+        {/* {props.value.description} */}
+        <SessionDetailInformation />
       </p>
+    </div>
+  );
+};
+
+const SessionDetailInformation = () => {
+  return (
+    <div className="flex items-center justify-center gap-2 leading-none">
+      <div className="bg-primary-500/30 rounded-default flex h-12 w-12 items-center justify-center">
+        <img className="scale-150" src={ThinkingIcon}></img>
+      </div>
+      <div className="flex flex-col gap-1">
+        <h3>Test</h3>
+        <p>Test your knowledge.</p>
+      </div>
     </div>
   );
 };
